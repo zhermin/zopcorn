92d550e2fb31db897f89a5da5c271b72a875762e
zhermin <tamzhermin@gmail.com>
2021-01-20 10:59:38 +0800

    Updates

---
+++ precache-manifest.c4f7d9852794943e5fdbb852f69e19e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52268a539ea3f0640925aeb786497c1c",
+    "url": "/zopcorn/index.html"
+  },
+  {
+    "revision": "bf861e4bb49e3006243f",
+    "url": "/zopcorn/static/css/main.706702d3.chunk.css"
+  },
+  {
+    "revision": "66c1ba6662af3933c6c2",
+    "url": "/zopcorn/static/js/2.f644d781.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zopcorn/static/js/2.f644d781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf861e4bb49e3006243f",
+    "url": "/zopcorn/static/js/main.b9109078.chunk.js"
+  },
+  {
+    "revision": "dff3e5fabb6aec11435d",
+    "url": "/zopcorn/static/js/runtime-main.ef71e93b.js"
+  }
+]);
